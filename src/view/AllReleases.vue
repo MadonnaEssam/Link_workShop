c217707c26d9237cca0c04b96b498b3f71af5fee
Madonna <52240034+MadonnaEssam@users.noreply.github.com>
2020-09-21 13:24:14 +0200

    Add select

--- src/view/AllReleases.vue
+++ src/view/AllReleases.vue
@@ -1,67 +1,67 @@
 <template>
 <div>
+    <v-app id="inspire">
+        <Navbar />
 
-    <Navbar />
+        <div class="page-margin">
 
-    <div class="page-margin">
+            <div class="breadcrumb-bg">
+                <div class="container">
+                    <div class="row">
 
-        <div class="breadcrumb-bg">
-            <div class="container">
-                <div class="row">
-
-                    <div class="col-md-6 col-sm-6 col-text-center">
-                        <nav aria-label="breadcrumb" class="blog-bradcrumb ">
-                            <ol class="breadcrumb bg-transparent mb-0">
-                                <li class="breadcrumb-item"><a :href='"/"'>Home</a></li>
-                                <li class="breadcrumb-item active"><a href="#">MediaCenter</a></li>
-                                <li class="breadcrumb-item active"><a href="#">Press Release</a></li>
-                            </ol>
-                        </nav>
-                    </div>
-                </div>
-            </div>
-        </div>
-
-        <section>
-            <v-container>
-                <h3 class="text-left"> Press Releases</h3>
-                <v-col cols="6">
-                    <select class="" style="display:block" v-model="selected" required="required" @change="onSelect($event)">
-                        <option v-for="(option, index) in states" v-bind:value="option.id" :key="index">{{ option.name }}</option>
-                    </select>
-                    <!-- <v-select v-model="e1" v-bind:items="states" label="Select" item-text="name" item-value="id" :hint="`${e1.name}, ${e1.id}`"></v-select> -->
-                </v-col>
-                <div class="review-block">
-                    <div class="container">
-                        <div class="row  margin-bottom">
-                            <div class="col-lg-4 col-md-6" v-for="(article,index) in articles" :key=" index">
-                                <div class="review-box" v-if="index<4" :style="{ backgroundImage:`linear-gradient(rgb(0 0 0 / 47%), rgb(0, 0, 0, 0.4)),url(${article.urlToImage})`  }">
-
-                                    <h5>{{article.publishedAt}}</h5>
-
-                                    <div class="review-message">
-                                        <p>{{article.title}}</p>
-                                    </div>
-                                    <div class="form-button">
-                                        <button type="button" class="btn btn-custom theme-color" @click="readMore(article.id)">Read More</button>
-                                    </div>
-                                </div>
-
-                            </div>
-
+                        <div class="col-md-6 col-sm-6 col-text-center">
+                            <nav aria-label="breadcrumb" class="blog-bradcrumb ">
+                                <ol class="breadcrumb bg-transparent mb-0">
+                                    <li class="breadcrumb-item"><a :href='"/"'>Home</a></li>
+                                    <li class="breadcrumb-item active"><a href="#">MediaCenter</a></li>
+                                    <li class="breadcrumb-item active"><a href="#">Press Release</a></li>
+                                </ol>
+                            </nav>
                         </div>
                     </div>
                 </div>
-            </v-container>
-        </section>
-        <Footer />
-    </div>
+            </div>
+
+            <section>
+                <v-container>
+                    <h3 class="text-left"> Press Releases</h3>
+                    <v-col cols="6">
+
+                        <v-select v-model="select" @change='changedLabel(select)' :items="items" item-text="name" item-value="id" label="Select" persistent-hint return-object single-line></v-select>
+                    </v-col>
+                    <div class="review-block">
+                        <div class="container">
+                            <div class="row  margin-bottom">
+                                <div class="col-lg-4 col-md-6" v-for="(article,index) in articles" :key=" index">
+                                    <div class="review-box" :style="{ backgroundImage:`linear-gradient(rgb(0 0 0 / 47%), rgb(0, 0, 0, 0.4)),url(${article.urlToImage})`  }">
+
+                                        <h5>{{article.publishedAt}}</h5>
+
+                                        <div class="review-message">
+                                            <p>{{article.title}}</p>
+                                        </div>
+                                        <div class="form-button">
+                                            <button type="button" class="btn btn-custom theme-color" @click="readMore(article.id)">Read More</button>
+                                        </div>
+                                    </div>
+
+                                </div>
+
+                            </div>
+                        </div>
+                    </div>
+                </v-container>
+            </section>
+            <Footer />
+        </div>
+    </v-app>
 </div>
 </template>
 
 <script>
 import json from '../../newsapi.json'
 import moment from "moment";
+
 export default {
     name: 'Releases',
     components: {
@@ -73,8 +73,11 @@ export default {
         return {
             myJson: json,
             articles: [],
-            states: [],
-            selected: null
+            items: [],
+            select: {
+                id: 1,
+                name: "TechCrunch"
+            }
 
         }
     },
@@ -83,10 +86,21 @@ export default {
         this.getDropDown()
     },
     methods: {
-        onSelect(e) {
-            var item = e.target.value
-            console.log(item)
+        changedLabel(e) {
+            this.getRelease()
+            console.log('e.name' + e.name + 'e.id' + e.id)
+            var y = this.articles;
+            this.articles = []
+            for (var i = 0; i < y.length; i++) {
+                if (y[i].sourceID == e.id) {
+                    y[i].publishedAt = moment(y[i].publishedAt).format('DD MMMM dddd,YYYY')
+            
+                    this.articles.push(y[i])
+                }
+            }
+
         },
+
         readMore(id) {
             this.$router.push({
                 name: 'ReleasesDetails',
@@ -97,7 +111,7 @@ export default {
         },
         getDropDown() {
             for (var y = 0; y < this.myJson.sourceCategory.length; y++) {
-                this.states.push(this.myJson.sourceCategory[y])
+                this.items.push(this.myJson.sourceCategory[y])
             }
         },
         getRelease() {
@@ -115,9 +129,3 @@ export default {
     },
 }
 </script>
-
-<style>
-.v-select {
-    display: grid;
-}
-</style>
